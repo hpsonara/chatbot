6c67a3dc6ae4623e4f435389debb435f114f755a
hitesh-ltimindtree <Hiteshchandra.Prakashchandrasonara@ltimindtree.com>
2023-08-18 10:28:21 +0530

    delete unnecessary code

--- ai-chat-bot-app-in-angular-using-chatGPT-main/README.md
+++
@@ -1,2 +0,0 @@
-# ai-chat-bot-app-in-angular-using-chatGPT
-This repo is created in Angular. I have made pages for ChatGPT demo and also a powerful AI Chat BOT using ChatGPT
--- ai-chat-bot-app-in-angular-using-chatGPT-main/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-import { EnvironmentConfiguration } from "src/app/models/environment-configuration";
-
-export const environment: EnvironmentConfiguration = {
-  env_name: 'prod',
-  production: true,
-  apiKey:''
-};
